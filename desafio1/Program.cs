66eb1cdcfe2529f70969e70fe08352be26e16a63
octavioduartejurczyszyn <tatoduarte9@gmail.com>
2024-02-20 19:17:05 +0100

    Primera subida

---
+++ desafio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono t1 = new Telefono("i2", "iphone" );
+            t1.CodigoOperador = 12;
+            t1.NumeroTelefonico = "698981284";
+            
+            Console.WriteLine(t1.NumeroTelefonico + t1.CodigoOperador + t1.Llamar("Tato"));
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ desafio1/Telefono.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace desafio1
+{
+    internal class Telefono
+    {
+        //private string modelo;
+        //private string marca;
+        //private string numeroTelefonico;
+        private int codigoOperador;
+
+        public string modelo { get; set; }
+        public string marca { get; set; }
+
+
+        //public string Modelo
+        //{
+        //    get { return modelo; }
+        //    set { modelo = value; }
+        //}
+        public string Marca { get; }
+        public string NumeroTelefonico { get; set; }
+        public int CodigoOperador 
+        {
+            get { return codigoOperador; }
+            set 
+            {
+                if (value == 1 || value == 2 || value == 3)
+                    codigoOperador = value;
+                else
+                    codigoOperador = 0;
+            }
+        }
+
+        // Cosntructor 
+        public Telefono (string modelo, string marca )
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+      
+        public string Llamar()
+        {
+            return "Realizando llamada...";
+        }
+
+        public string Llamar (string contacto)
+        {
+            return "Llamando a " + contacto;
+        }
+
+        //Crear algunos objetos en el main de Program y probar escribirle datos y mostrar en pantalla el estado del Telefono.
+        //Agregar método Llamar() sin parámetros que devuelva un string con la leyenda "Realizando llamada...".
+        //Sobrecargar el método Llamar(string contacto) para que reciba un contacto y devuelva un string con la leyenda "Llamando a " + contacto
+        //Probar métodos en el main mostrando en pantalla el comportamiento de los objetos
+    }
+}
